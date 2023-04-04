dbc8305d33df2e72b0e415743bc577c2f0b4f0ce
hong-yeong-gwang <chamsk0808@naver.com>
2023-04-04 22:22:28 +0900

    Initial commit

---
+++ src/main/java/com/ybus/ybusapiserver/YbusApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.ybus.ybusapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbusApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbusApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ybus/ybusapiserver/YbusApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybus.ybusapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbusApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
